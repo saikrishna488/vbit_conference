efa9d2dbd820f1e722ef6a3c89ebe09ed41e2768
Saikrishna Reddy <117559736+saikrishna488@users.noreply.github.com>
2024-02-29 05:17:40 +0000

    14th commit

---
+++ app/components/Conference.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Conference = () => {
+    return (
+        <div className="bg-white p-8">
+            <h2 className="text-2xl font-bold text-orange-500 mb-4">About Conference</h2>
+            <p className="text-gray-700">
+                The conference "ic-ETITE’24" emphasizes the critical need for advancing research in Information Technology and Engineering. It serves as a catalyst for global collaboration, providing researchers with a platform to showcase their groundbreaking innovations in the realm of science and technology. The primary goal is to foster and enhance research and development initiatives within the domains of Information Technology and Engineering.
+
+                By facilitating networking and knowledge exchange, ic-ETITE’24 aims to propel the academic community towards collective progress. It seeks to empower researchers by enabling them to forge international connections, thereby fostering collaborative endeavors that transcend geographical boundaries. Through this collaborative approach, the conference endeavors to drive innovation and elevate the standards of research within the field.
+
+                With a mission to cultivate a culture of excellence and innovation, ic-ETITE’24 is committed to nurturing the next generation of pioneers in Information Technology and Engineering. By providing a platform for researchers to share their insights and discoveries, the conference aims to inspire new ideas and perspectives that will shape the future of these disciplines. Ultimately, the overarching objective is to facilitate the establishment of global linkages that will catalyze transformative advancements in academia and beyond.
+            </p>
+            <p className="text-gray-700 mt-4">
+                The primary objective of this conference is to advance research across various domains, including Information Technology, Computer Engineering, Communication Engineering, and Electronics Engineering. It serves as an international platform for scholars, educators, engineers, industry professionals, and students worldwide to disseminate their research findings and engage with leading experts in the field of Science and Technology.
+
+                At its core, the conference strives to facilitate the establishment of fruitful collaborations and networking opportunities for delegates. By fostering interactions among participants, it aims to catalyze the initiation of research projects and business ventures, thereby propelling individuals along their career trajectories.
+
+                The call for papers welcomes original and unpublished contributions that highlight specific research areas from diverse subject perspectives. Delegates from around the globe are invited to submit their work, reflecting the conference's commitment to inclusivity and diversity within the research community.
+
+                With a focus on knowledge exchange and interdisciplinary collaboration, the conference endeavors to deliver a comprehensive and enriching experience for all attendees. By providing a forum for the dissemination of cutting-edge research and fostering dialogue among stakeholders, it aims to contribute to the advancement of scientific knowledge and technological innovation on a global scale.
+            </p>
+        </div>
+    );
+};
+
+export default Conference;
--- app/conference/page.js
+++ app/conference/page.js
@@ -1,7 +1,14 @@
+import Conference from "../components/Conference";
+import Footer from "../components/Footer";
+import SponsorsSection from "../components/SponsersSection";
 
 
 export default function Home(){
     return (
-        <></>
+        <>
+        <Conference/>
+        <SponsorsSection/>
+        <Footer/>
+        </>
     )
 }
