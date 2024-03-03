bda7d4f4f78870fd02e7d041e1e0de587078951c
Saikrishna Reddy <117559736+saikrishna488@users.noreply.github.com>
2024-03-03 15:06:07 +0000

    15th commit

--- app/components/Committee.jsx
+++ app/components/Committee.jsx
@@ -4,12 +4,34 @@ const CommitteeSection = () => {
     return (
         <div className="bg-white py-8 px-4">
             <h2 className="text-orange-500 text-2xl font-bold mb-4">Organizing Committee</h2>
-            
+
             <div className="mb-6">
                 <div className="flex justify-between">
                     <div className="w-1/2 pr-2">
-                        <h3 className="text-orange-500 text-lg font-semibold">Chief Patron</h3>
-                        <p>Dr. G. Viswanathan, Chancellor, VIT</p>
+                        <h3 className="text-orange-500 text-lg font-semibold">Technical program</h3>
+                        <br />
+                        <p>Ming Yang, Kennesaw State University, Atlanta, USA</p>
+                        <p>Sanjay Misra</p>
+                        <p>Milos Stojmenovic</p>
+                        <p>Georgios Tsaramirsis</p>
+                        <p>Sven Groppe</p>
+                        <p>Mohammad Yamin</p>
+                        <p>Mukesh Misra</p>
+                        <p>Neha Mohanty</p>
+                        <p>Jinghua Groppe</p>
+                        <p>Abdulrhman Alshareef</p>
+                        <p>Gourav Gupta</p>
+                        <p>Lal Hussain</p>
+                        <p>María Avilés</p>
+                        <p>Izzat Al-Darraji</p>
+                        <p>Lie Li</p>
+                        <p>Abdulsattar Abdullah Hamad</p>
+                        <p>Vicente Garcia Diaz</p>
+                        <p>Oscar Castillo</p>
+                        <p>J P Das</p>
+                        <p>Rajkumar Singh Rathore</p>
+                        <p>Dr. Prasad Chetti, Northwest Missouri State University, USA</p>
+
                     </div>
                     <div className="w-1/2 pl-2">
                         <h3 className="text-orange-500 text-lg font-semibold">Patrons</h3>
@@ -24,7 +46,7 @@ const CommitteeSection = () => {
                     </div>
                 </div>
             </div>
-            
+
             <div className="mb-6">
                 <div className="flex justify-between">
                     <div className="w-1/2 pr-2">
@@ -37,8 +59,6 @@ const CommitteeSection = () => {
                     </div>
                 </div>
             </div>
-
-            {/* Add more sections similarly */}
 
         </div>
     );
